e8c3d71a202a0d498495510bae64dd925031bd3e
total-babe <kgwaleflorence@yahoo.com>
2023-01-06 10:34:18 +0200

    weather data analysis

---
+++ practice.py
@@ -0,0 +1,16 @@
+second_list= list("Life is a study")
+print(second_list)
+empty_list=[]
+print(empty_list)
+empty_list.append("Im no longer empty")
+print(empty_list)
+second_list.remove
+num_list=[1, 2, 3, 4, 6, 7, 8]
+print(len(num_list))
+print(sum(num_list))
+5 in num_list
+my_tuple=(1, 2, 3)
+print(my_tuple)
+my_string = "Hello world"
+print(my_string)
+import numpy as np
